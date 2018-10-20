ee43575d639c42de729930702d29b9aa21b22d80
Eric Pinzur <epinzur@gmail.com>
2018-10-20 00:32:12 -0500

    can attach and stay connected to speaker

---
+++ socket.js
@@ -0,0 +1,40 @@
+var net = require('net');
+
+var init_request = Buffer.from('554300010800554d00006400cfde', "hex");
+//'UC§JMfd{"id": "Subscribe","clientName": "SL Room Control","clientType": "Mac","clientDescription": "Eric\'s MacBook Air","clientIdentifier": "Ericâs MacBook Air"}UCKAfd'
+var subscription_request = Buffer.from('55430001a7004a4d660064009d0000007b226964223a2022537562736372696265222c22636c69656e744e616d65223a2022534c20526f6f6d20436f6e74726f6c222c22636c69656e7454797065223a20224d6163222c22636c69656e744465736372697074696f6e223a2022457269635c2773204d6163426f6f6b20416972222c22636c69656e744964656e746966696572223a202245726963e2809973204d6163426f6f6b20416972227d5543000106004b4166006400', "hex");
+
+var keep_alive = Buffer.from('5543000106004b4166006400', 'hex');
+
+var client = new net.Socket({'allowHalfOpen': true});
+client.connect(48207, '192.168.0.100', function() {
+    console.log('Connected');
+    client.write(init_request);
+    setTimeout(function()  {
+        console.log('Sending 2nd command now');
+        client.write(subscription_request);
+        console.log('2nd command sent');
+    }, 100);
+    setTimeout(function()  {
+        console.log('starting keep alive');
+        setInterval(function() {
+            client.write(keep_alive);
+            console.log('keep alive sent')
+        }, 3000)
+    }, 500);
+    console.log('Data written');
+});
+
+client.on('data', function(data) {
+    console.log('Data Received: ');
+    console.log(data);
+});
+
+client.on('close', function() {
+    console.log('Connection closed');
+});
+
+client.on('error', function(error) {
+    console.log('ERROR');
+    console.log(error);
+});

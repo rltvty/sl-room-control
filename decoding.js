fc963d3b0ed1f2d4deb47672907adcb013fbbe49
Eric Pinzur <epinzur@gmail.com>
2018-10-20 02:55:08 -0500

    tryign to figure out levels, unsuccessfully

---
+++ decoding.js
@@ -0,0 +1,248 @@
+/*
+COMMANDS IN HEX, FROM PREVIOUS RESEARCH
+ 
+ **** Sent this command to set master to 7 db:
+ 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:3b:bd:67:3f
+
+ **** Sent this command to set master to 4 db:
+ 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:17:14:51:3f
+
+
+ **** Sent this command to set master to 0 db:
+ 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:7f:41:35:3f
+
+
+ **** Sent this command to set master to -5 db:
+ 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:50:22:16:3f
+
+
+ **** Sent this command to set master to -9 db:
+ 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:00:00:00:3f
+
+ -8.99 db: 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:66:0d:00:3f
+ -8.98 db: 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:d3:1a:00:3f
+ -8.97 db: 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:40:28:00:3f
+
+ **** Dragged to set master to -35.14 db:
+ 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:ca:e0:fc:3e
+
+ 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:91:c1:f9:3e55:43:00:01:06:00:4b:41:65:00:64:00
+
+ 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:1e:83:f3:3e
+
+ 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:75:25:ea:3e55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:5e:89:da:3e
+
+ 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:b6:2b:d1:3e55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:66:70:be:3e55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:89:f3:b1:3e
+
+ 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:12:b5:ab:3e55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:8a:da:95:3e55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:19:9c:8f:3e
+
+ 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:a9:5d:89:3e
+
+ 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:39:1f:83:3e55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:20:83:73:3e55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:b0:44:6d:3e55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:7d:0c:4e:3e
+
+ 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:0f:ce:47:3e55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:2c:51:3b:3e
+
+ 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:bd:12:35:3e
+
+ 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:4d:d4:2e:3e55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:dd:95:28:3e
+
+ 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:6d:57:22:3e55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:fa:18:1c:3e
+
+ 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:8a:da:15:3e55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:1a:9c:0f:3e
+
+ 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65:00:00:00:a9:5d:09:3e
+
+
+ */
+
+/* REMOVE WHAT I THINK IS THE URL: 55:43:00:01:24:00:50:56:65:00:64:00:53:70:65:61:6b:65:72:2f:6c:69:6e:65:2f:63:68:31:2f:76:6f:6c:75:6d:65
+   ALSO REMOVE THE KEEP ALIVE: 55:43:00:01:06:00:4b:41:65:00:64:00
+
+ **** Sent this command to set master to 7 db:
+  00:00:00:3b:bd:67:3f
+
+ **** Sent this command to set master to 4 db:
+  00:00:00:17:14:51:3f
+
+
+ **** Sent this command to set master to 0 db:
+  00:00:00:7f:41:35:3f
+
+
+ **** Sent this command to set master to -5 db:
+  00:00:00:50:22:16:3f
+
+
+ **** Sent this command to set master to -9 db:
+  00:00:00:00:00:00:3f
+
+ **** Dragged to set master to -35.14 db:
+  00:00:00:ca:e0:fc:3e
+
+  00:00:00:91:c1:f9:3e
+
+  00:00:00:1e:83:f3:3e
+
+  00:00:00:75:25:ea:3e 00:00:00:5e:89:da:3e
+
+  00:00:00:b6:2b:d1:3e 00:00:00:66:70:be:3e 00:00:00:89:f3:b1:3e
+
+  00:00:00:12:b5:ab:3e 00:00:00:8a:da:95:3e 00:00:00:19:9c:8f:3e
+
+  00:00:00:a9:5d:89:3e
+
+  00:00:00:39:1f:83:3e 00:00:00:20:83:73:3e 00:00:00:b0:44:6d:3e 00:00:00:7d:0c:4e:3e
+
+  00:00:00:0f:ce:47:3e 00:00:00:2c:51:3b:3e
+
+  00:00:00:bd:12:35:3e
+
+  00:00:00:4d:d4:2e:3e 00:00:00:dd:95:28:3e
+
+  00:00:00:6d:57:22:3e 00:00:00:fa:18:1c:3e
+
+  00:00:00:8a:da:15:3e 00:00:00:1a:9c:0f:3e
+
+  00:00:00:a9:5d:09:3e
+ */
+
+/* CREATE VARS FOR THESE THINGS */
+
+/*
+var statics = {
+    "7":      Buffer.from("0000003bbd673f", 'hex'),
+    "4":      Buffer.from("0000001714513f", 'hex'),
+    "0":      Buffer.from("0000007f41353f", 'hex'),
+    "-5":     Buffer.from("0000005022163f", 'hex'),
+    "-9":     Buffer.from("0000000000003f", 'hex'),
+    "-35.14": Buffer.from("000000a95d093e", 'hex')
+};
+
+console.log(Object.keys(statics));
+
+var moving_down = [
+    Buffer.from("000000cae0fc3e", 'hex'),
+    Buffer.from("00000091c1f93e", 'hex'),
+    Buffer.from("0000001e83f33e", 'hex'),
+    Buffer.from("0000007525ea3e", 'hex'),
+    Buffer.from("0000005e89da3e", 'hex'),
+    Buffer.from("000000b62bd13e", 'hex'),
+    Buffer.from("0000006670be3e", 'hex'),
+    Buffer.from("00000089f3b13e", 'hex'),
+    Buffer.from("00000012b5ab3e", 'hex'),
+    Buffer.from("0000008ada953e", 'hex'),
+    Buffer.from("000000199c8f3e", 'hex'),
+    Buffer.from("000000a95d893e", 'hex'),
+    Buffer.from("000000391f833e", 'hex'),
+    Buffer.from("0000002083733e", 'hex'),
+    Buffer.from("000000b0446d3e", 'hex'),
+    Buffer.from("0000007d0c4e3e", 'hex'),
+    Buffer.from("0000000fce473e", 'hex'),
+    Buffer.from("0000002c513b3e", 'hex'),
+    Buffer.from("000000bd12353e", 'hex'),
+    Buffer.from("0000004dd42e3e", 'hex'),
+    Buffer.from("000000dd95283e", 'hex'),
+    Buffer.from("0000006d57223e", 'hex'),
+    Buffer.from("000000fa181c3e", 'hex'),
+    Buffer.from("0000008ada153e", 'hex'),
+    Buffer.from("0000001a9c0f3e", 'hex')
+];
+
+
+for (var offset = 0; offset < 12; offset += 1) {
+    console.log('');
+    console.log('TESTING OFFSET: ' + offset);
+    for (var key in statics) {
+        var item = statics[key];
+
+        try {
+            console.log('\t is key: ' + key + ' == readDoubleBE: ' + item.readDoubleBE(offset));
+        } catch (e) {
+        }
+        try {
+            console.log('\t is key: ' + key + ' == readDoubleLE: ' + item.readDoubleLE(offset));
+        } catch (e) {
+        }
+        try {
+            console.log('\t is key: ' + key + ' == readFloatBE: ' + item.readFloatBE(offset));
+        } catch (e) {
+        }
+        try {
+            console.log('\t is key: ' + key + ' == readFloatLE: ' + item.readFloatLE(offset));
+        } catch (e) {
+        }
+
+        try {
+            console.log('\t is key: ' + key + ' == readInt8: ' + item.readInt8(offset));
+        } catch (e) {
+        }
+        try {
+            console.log('\t is key: ' + key + ' == readInt16BE: ' + item.readInt16BE(offset));
+        } catch (e) {
+        }
+        try {
+            console.log('\t is key: ' + key + ' == readInt16LE: ' + item.readInt16LE(offset));
+        } catch (e) {
+        }
+        try {
+            console.log('\t is key: ' + key + ' == readInt32BE: ' + item.readInt32BE(offset));
+        } catch (e) {
+        }
+        try {
+            console.log('\t is key: ' + key + ' == readInt32LE: ' + item.readInt32LE(offset));
+        } catch (e) {
+        }
+    }
+}
+*/
+
+/* Pretty sure not using int.
+ buf.readInt8(offset)
+ buf.readInt16BE(offset)
+ buf.readInt16LE(offset)
+ buf.readInt32BE(offset)
+ buf.readInt32LE(offset)
+ buf.readIntBE(offset, byteLength)
+ buf.readIntLE(offset, byteLength)
+ buf.readUInt8(offset)
+ buf.readUInt16BE(offset)
+ buf.readUInt16LE(offset)
+ buf.readUInt32BE(offset)
+ buf.readUInt32LE(offset)
+ buf.readUIntBE(offset, byteLength)
+ buf.readUIntLE(offset, byteLength)
+ */
+
+/* NOT THE RESULTS I WANTED.  MAYBE ITS JUST A LINEAR REGRESSION? */
+
+//   Y        X
+var values = {
+    "7":      parseInt("0000003bbd673f", 16),
+    "4":      parseInt("0000001714513f", 16),
+    "0":      parseInt("0000007f41353f", 16),
+    "-5":     parseInt("0000005022163f", 16),
+    "-9":     parseInt("0000000000003f", 16),
+    "-35.14": parseInt("000000a95d093e", 16)
+};
+
+console.log('Values:');
+console.log(values);
+
+var m = (4-7)/(values["4"] - values["7"]);
+
+var b = 7 - (m * values["7"]);
+
+console.log('Equation: y=' + m + 'x + ' + b);
+
+function y(x) {
+    return (m*x) + b;
+}
+
+console.log(' 7 is ' + y(values["7"]));
+console.log(' 4 is ' + y(values["4"]));
+console.log(' 0 is ' + y(values["0"]));
+console.log('-5 is ' + y(values["-5"]));
+console.log('-9 is ' + y(values["-9"]));
+console.log('-35.14 is ' + y(values["-35.14"]));
+
+/* DID I DO THIS WRONG? SEEMS LIKE NOT LINEAR REGRESSION EITHER :( */
